546ee0a4c39a1da11ef4ca9ba912b8114cd3344b
QMinh-2005 <quanminh88098@gmail.com>
2025-04-06 23:58:06 +0700

    thông tin đã sửa

---
+++ Form3.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Bai17_Listview
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sadsadsadsadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sadasdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sadsadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sadsadsadsadToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 28);
+            // 
+            // sadsadsadsadToolStripMenuItem
+            // 
+            this.sadsadsadsadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sadasdToolStripMenuItem});
+            this.sadsadsadsadToolStripMenuItem.Name = "sadsadsadsadToolStripMenuItem";
+            this.sadsadsadsadToolStripMenuItem.Size = new System.Drawing.Size(170, 24);
+            this.sadsadsadsadToolStripMenuItem.Text = "sadsadsadsad";
+            // 
+            // sadasdToolStripMenuItem
+            // 
+            this.sadasdToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sadsadToolStripMenuItem});
+            this.sadasdToolStripMenuItem.Name = "sadasdToolStripMenuItem";
+            this.sadasdToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sadasdToolStripMenuItem.Text = "sadasd";
+            // 
+            // sadsadToolStripMenuItem
+            // 
+            this.sadsadToolStripMenuItem.Name = "sadsadToolStripMenuItem";
+            this.sadsadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sadsadToolStripMenuItem.Text = "sadsad";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sadsadsadsadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sadasdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sadsadToolStripMenuItem;
+    }
+}
